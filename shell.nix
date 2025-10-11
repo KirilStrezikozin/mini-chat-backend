0d8baf2e37ab2b5421078d375ecae7cb3e4094d1
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-10-11 23:17:35 +0200

    shell.nix: add adminer, psql to deps, source venv
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- shell.nix
+++ shell.nix
@@ -3,9 +3,27 @@ pkgs.mkShell {
   packages = with pkgs; [
     pre-commit
 
+    tree
+
     nixd
     poetry
     ruff
     pyright
+
+    caddy
+    postgresql_17
+    adminer
+    (writeShellApplication {
+      name = "adminer-serve";
+      text = ''
+        #!/bin/sh
+        cd ${adminer} || exit 1
+        nix-shell -p php --command 'php -S localhost:9001'
+      '';
+    })
   ];
+
+  shellHook = ''
+    source $(poetry env info --path)/bin/activate
+  '';
 }
