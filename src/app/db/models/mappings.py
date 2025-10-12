5ab98a168a0ff7a51ec0ffbdc10be7c270147b96
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-10-12 23:15:53 +0200

    db/models: fix timestamp date type, run migrations
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- src/app/db/models/mappings.py
+++ src/app/db/models/mappings.py
@@ -1,11 +1,14 @@
 import uuid
-from datetime import UTC, datetime
+from datetime import datetime
 from typing import Annotated
 
+from sqlalchemy import DateTime, func
 from sqlalchemy.orm import mapped_column
 
 from app.utils.types import IDType
 
 PrimaryKeyID = Annotated[IDType, mapped_column(default=uuid.uuid4, primary_key=True)]
 
-Timestamp = Annotated[datetime, mapped_column(default=datetime.now(UTC))]
+Timestamp = Annotated[
+    datetime, mapped_column(DateTime(timezone=True), default=func.now())
+]
